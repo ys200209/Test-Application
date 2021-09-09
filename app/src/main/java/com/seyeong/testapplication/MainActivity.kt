886ab863fbb381065f1838664e6627dce0d900e0
ys200209 <lys200209@naver.com>
2021-09-09 15:29:55 +0900

    Create Load Music Playlist

--- app/src/main/java/com/seyeong/testapplication/MainActivity.kt
+++ app/src/main/java/com/seyeong/testapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.seyeong.testapplication
 import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.seyeong.testapplication.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
@@ -26,6 +28,41 @@ class MainActivity : BaseActivity() {
     }
 
     fun startProcess() {
+        val adapter = MusicRecyclerAdapter()
+        adapter.musicList.addAll(getMusicList())
 
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
+    fun getMusicList(): List<Music> {
+        val listUrl = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI // 음원 정보의 주소를 받아온다.
+
+        val proj = arrayOf( // 음원 정보 테이블에서 읽어올 컬럼명
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ALBUM_ID,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val cursor = contentResolver.query(listUrl, proj, null, null, null)
+        // ContentResolver의 query() 메서드에 주소와 컬럼명을 담아 호츌한다.
+
+        val musicList = mutableListOf<Music>()
+
+        while(cursor?.moveToNext() == true) {
+            val id = cursor.getString(0)
+            val title = cursor.getString(1)
+            val artist = cursor.getString(2)
+            val albumId = cursor.getString(3)
+            val duration = cursor.getLong(4)
+
+            val music = Music(id, title, artist, albumId, duration)
+            musicList.add(music)
+        }
+
+        return musicList
+    }
+
 }
--- app/src/main/java/com/seyeong/testapplication/MusicRecyclerAdapter.kt
+++ app/src/main/java/com/seyeong/testapplication/MusicRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.seyeong.testapplication
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -7,8 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.seyeong.testapplication.databinding.ItemRecyclerBinding
 import java.text.SimpleDateFormat
 
-class MusicRecyclerAdapter: RecyclerView.Adapter<Holder>() {
-    val musicList = mutableListOf<Music>()
+class MusicRecyclerAdapter: RecyclerView.Adapter<MusicRecyclerAdapter.Holder>() {
+    var musicList = mutableListOf<Music>()
+    var mediaPlayer: MediaPlayer? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -25,21 +27,21 @@ class MusicRecyclerAdapter: RecyclerView.Adapter<Holder>() {
         holder.setMusic(music)
     }
 
+    inner class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+        var musicUri: Uri? = null
 
-}
+        fun setMusic(music: Music) {
+            binding.run{
+                imageAlbum.setImageURI(music.getAlbumUri()) // Music 클래스에 정의된 getAlbumUri 메서드 호출
+                textArtist.text = music.artist
+                textTitle.text = music.title
 
-class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
-    var musicUri: Uri? = null
-
-    fun setMusic(music: Music) {
-        binding.run{
-            imageAlbum.setImageURI(music.getAlbumUri()) // Music 클래스에 정의된 getAlbumUri 메서드 호출
-            textArtist.text = music.artist
-            textTitle.text = music.title
-
-            val duration = SimpleDateFormat("mm:ss").format(music.duration)
-            textDuration.text = duration
+                val duration = SimpleDateFormat("mm:ss").format(music.duration)
+                textDuration.text = duration
+            }
+            this.musicUri = music.getMusicUri()
         }
-        this.musicUri = music.getMusicUri()
     }
+
 }
+
